dbb44b8155aed179fa2be675575f47d3b6861c98
Martin Toman <toman.martin@live.com>
2021-07-28 09:46:13 -0700

    Remove flake-utils, define flake directly

--- flake.nix
+++ flake.nix
@@ -1,32 +1,40 @@
 {
   inputs = {
-    flake-utils.url = github:numtide/flake-utils;
     nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
     mach-nix = {
       url = github:DavHau/mach-nix/master;
       inputs = {
-        nixpkgs.follows     = "nixpkgs";
-        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
       };
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, mach-nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs {
-          inherit system;
-          overlays = [ ];
-        });
-      in rec {
-        devShell = import ./shell.nix { inherit pkgs; };
-        packages = {
+  outputs = { self, nixpkgs, mach-nix }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+
+      # Function to generate a set based on supported systems
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ ];
+      });
+
+    in rec {
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
           rmapi = pkgs.rmapi;
           paper2remarkable = (import ./paper2remarkable {
             inherit pkgs;
             mach-nix = (mach-nix.lib.${system});
           });
-        };
-        overlay = final: prev: packages;
+        });
+
+      devShell = forAllSystems (system: import ./shell.nix {
+        pkgs = nixpkgsFor.${system};
       });
+    };
 }
